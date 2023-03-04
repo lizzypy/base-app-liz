d9e034701684b654045388ddb551e6f8cfbd7034
Liz Johnson <liz.johnson@movehq.com>
2023-03-04 14:53:43 -0800

    backend users post works

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,22 +1,97 @@
-import { useEffect } from 'react';
-import { useUser } from './useUser';
+import React, {
+    createContext,
+    useContext,
+    useEffect,
+    useMemo,
+    useState,
+} from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-export const useAuth = () => {
-    const { user, addUser, removeUser } = useUser();
+export function AuthProvider({setAuth, children}) {
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [loadingInitial, setLoadingInitial] = useState(true);
+
+    const history = useHistory();
+    const location = useLocation();
 
     useEffect(() => {
-        if (user) {
-            addUser(JSON.parse(user));
-        }
+        if (error) setError(null);
+    }, [location.pathname]);
+
+    useEffect(() => {
+        usersApi.getCurrentUser()
+            .then((user) => setUser(user))
+            .catch((_error) => {})
+            .finally(() => setLoadingInitial(false));
     }, []);
 
-    const login = (user) => {
-        addUser(user);
-    };
+    // Flags the component loading state and posts the login
+    // data to the server.
+    //
+    // An error means that the email/password combination is
+    // not valid.
+    //
+    // Finally, just signal the component that loading the
+    // loading state is over.
+    function login(email, password) {
+        setLoading(true);
 
-    const logout = () => {
-        removeUser();
-    };
+        sessionsApi.login({ email, password })
+            .then((user) => {
+                setUser(user);
+                history.push("/");
+            })
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }
 
-    return { user, login, logout };
-};
+    function signUp(email, password) {
+        setLoading(true);
+
+        usersApi.signUp({ email, password })
+            .then((user) => {
+                setUser(user);
+                history.push("/");
+            })
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }
+
+    function logout() {
+        sessionsApi.logout().then(() => setUser(undefined));
+    }
+
+    // Make the provider update only when it should.
+    // We only want to force re-renders if the user,
+    // loading or error states change.
+    //
+    // Whenever the `value` passed into a provider changes,
+    // the whole tree under the provider re-renders, and
+    // that can be very costly! Even in this case, where
+    // you only get re-renders when logging in and out
+    // we want to keep things very performant.
+    setAuth(
+        {
+            user,
+            loading,
+            error,
+            login,
+            signUp,
+            logout
+        })
+    // We only want to render the underlying app after we
+    // assert for the presence of a current user.
+    return (
+        <AuthContext.Provider value={memoedValue}>
+            {!loadingInitial && children}
+        </AuthContext.Provider>
+    );
+}
+
+// Let's only export the `useAuth` hook instead of the context.
+// We only want to use the hook directly and never the context component.
+export default function useAuth() {
+    return useContext(AuthContext);
+}
--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
-set -eou pipefail
+set -euo pipefail
 
 TMPDIR="/usr/src/app/tmp/"
 if [ -d "$TMPDIR" ]; then rm -rf "$TMPDIR"; fi
 
 rm -f /myapp/tmp/pids/server.pid
 
-rake db:migrate db:seed
+rake db:create db:migrate db:seed
 
-rails server -b 0.0.0.0 -p $PORT
+rails server -b 0.0.0.0
