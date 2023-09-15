e9834fd5267c402d3a410a16a43a3c53a224dae9
Liz Johnson <lizjohnson@lizs-mbp.lan>
2023-09-15 17:04:23 -0500

    Participants page with toolbar

--- frontend/src/pages/Participants.tsx
+++ frontend/src/pages/Participants.tsx
@@ -1,5 +1,27 @@
 import ParticipantsList from "../components/ParticipantsList";
+import {AppBar, Toolbar } from "@mui/material";
+import SignUpForm from "../components/signUpForm";
 
 export const Participants: NextPage = () => {
-  return <ParticipantsList/>
+  return (
+          <>
+            <AppBar position="static" sx={{ bgcolor: "pink" }}>
+                <section aria-label={"toolbar-section"}
+                         style={{display: "flex", flexDirection: "row", justifyContent: "right"}}>
+                    <Toolbar>
+                        <SignUpForm/>
+                    </Toolbar>
+                </section>
+            </AppBar>
+            <section aria-label={"participants-section"}
+                     style={{
+                         padding: "40px 30px",
+                         display: "flex",
+                         flexDirection: "column",
+                         justifyContent: "space-around"
+                     }}>
+              <ParticipantsList/>
+            </section>
+          </>
+    )
 }
