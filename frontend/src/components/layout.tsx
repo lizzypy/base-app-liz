9219e5ffbb7d62a35ef1d0997beec1453e440c44
Liz Johnson <lizjohnson@lizs-mbp.lan>
2023-09-16 22:07:00 -0500

    use global nav bar layout

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,9 +1,17 @@
 import Navbar from './Navbar'
+import {AppBar, Toolbar } from "@mui/material";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Navbar/>
+      <AppBar position="static" sx={{ bgcolor: "pink" }}>
+                <section aria-label={"toolbar-section"}
+                         style={{display: "flex", flexDirection: "row", justifyContent: "right"}}>
+                    <Toolbar>
+                      <Navbar/>
+                    </Toolbar>
+                </section>
+      </AppBar>
       <main>{children}</main>
     </>
   )
